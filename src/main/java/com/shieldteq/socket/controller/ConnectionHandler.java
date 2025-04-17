de35f7c144b5faeab8193c383572659a629cb4c8
Ranjan <ranjanjena124@gmail.com>
2025-04-17 11:47:30 +0200

    Establish Event Connection

--- src/main/java/com/shieldteq/socket/controller/ConnectionHandler.java
+++ src/main/java/com/shieldteq/socket/controller/ConnectionHandler.java
@@ -12,14 +12,19 @@ import reactor.core.publisher.Mono;
 public class ConnectionHandler {
     private final MathClientManager mathClientManager;
 
-    //    @ConnectMapping
+//    @ConnectMapping
 //    public Mono<Void> handleConnection(ClientConnectionRequest request, RSocketRequester requester) {
 //        System.out.println("Connection established : " + request);
 //        return "secret".equals(request.secret()) ? Mono.empty() : Mono.fromRunnable(() -> requester.rsocketClient().dispose());
 //    }
     @ConnectMapping
+    public Mono<Void> noEventConnection(RSocketRequester requester) {
+        System.out.println("No event connection established");
+        return Mono.empty();
+    }
+    @ConnectMapping("math.events.connection")
     public Mono<Void> handleConnection(RSocketRequester requester) {
-        System.out.println("Connection established");
+        System.out.println("Event Connection established");
         return Mono.fromRunnable(() -> mathClientManager.addClient(requester));
     }
 }
--- src/test/java/com/shieldteq/socket/ConnectionRetryTest.java
+++ src/test/java/com/shieldteq/socket/ConnectionRetryTest.java
@@ -1,12 +1,15 @@
 package com.shieldteq.socket;
 
 import com.shieldteq.socket.dto.ComputationRequestDto;
+import com.shieldteq.socket.dto.ComputationResponseDto;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.test.context.TestPropertySource;
+import reactor.test.StepVerifier;
+import reactor.util.retry.Retry;
 
 import java.time.Duration;
 
@@ -14,20 +17,26 @@ import java.time.Duration;
 @TestPropertySource(properties = {
         "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.rsocket.RSocketServerAutoConfiguration"
 })
-class ConnectionManagerTest {
+class ConnectionRetryTest {
     @Autowired
     private RSocketRequester.Builder builder;
 
     @Test
     public void connectionSetupTest() throws InterruptedException {
         RSocketRequester requester1 = builder
+                .rsocketConnector(c -> c.reconnect(Retry.fixedDelay(50, Duration.ofSeconds(2))
+                        .doBeforeRetry(r -> System.out.println("retrying connection: " + r.totalRetriesInARow()))))
                 .transport(TcpClientTransport.create("localhost", 6565));
-        RSocketRequester requester2 = builder
-                .setupRoute("math.events.connection")
-                .transport(TcpClientTransport.create("localhost", 6565));
-        requester1.route("math.service.print").data(ComputationRequestDto.builder().input(5).build()).send().subscribe();
-        requester2.route("math.service.print").data(ComputationRequestDto.builder().input(5).build()).send().subscribe();
+        for (int i = 0; i < 50; i++) {
+            requester1.route("math.service.square")
+                    .data(ComputationRequestDto.builder().input(5).build())
+                    .retrieveMono(ComputationResponseDto.class)
+                    .doOnNext(System.out::println)
+                    .as(StepVerifier::create)
+                    .expectNextCount(1)
+                    .verifyComplete();
+            Thread.sleep(Duration.ofSeconds(2));
+        }
 
-        Thread.sleep(Duration.ofSeconds(10));
     }
 }
